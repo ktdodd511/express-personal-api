19300402ac84c98dec099dd3511195514ce200ab
ktdodd511 <katiedodd@Katies-MacBook-Air.local>
2016-11-12 12:57:13 -0800

    incorporated handlebars, started some styling, attemping ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,41 @@
 console.log("Sanity Check: JS is working!");
 
+var allAnime = [];
+
 $(document).ready(function(){
 
-// your code
+var $animeList = $('#animeTarget');
+
+// handlebars template
+var source = $('#anime-template').html();
+var template = Handlebars.compile(source);
+
+$.ajax({
+  method: 'GET',
+  url: '/api/anime',
+  success: handleSuccess,
+  error: handleError
+});
+
+
+function render () {
+  $animeList.empty();
+
+  // pass 'allAnime' into the template function
+  var animeHtml = template({ anime: allAnime });
+
+  // append html to view
+  $animeList.append(animeHtml);
+}
+
+function handleSuccess(json) {
+  allAnime = json;
+  render();
+}
+
+function handleError(err) {
+  console.log('oh no!');
+  $('#animeTarget').text('Failed to load anime shows.');
+}
 
 });
